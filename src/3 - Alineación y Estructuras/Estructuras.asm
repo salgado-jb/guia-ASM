32d1d62d5f291a74a9498665c814abee11f7504e
salgado-jb <salgado.jbs@gmail.com>
2025-04-30 03:35:44 -0300

    Termino offsets

--- src/3 - Alineación y Estructuras/Estructuras.asm
+++ src/3 - Alineación y Estructuras/Estructuras.asm
@@ -3,18 +3,18 @@
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
 NODO_OFFSET_NEXT EQU 0;Una struct con un puntero unicamente: 8 bytes
-NODO_OFFSET_CATEGORIA EQU 8;uint8_t o sea 1 byte (padding: 3 bytes)
-NODO_OFFSET_ARREGLO EQU 8;uint32_t o sea 4 bytes (no hace falta padding)
-NODO_OFFSET_LONGITUD EQU 12;uint32_t o sea 4 bytes (padding: 4 bytes)
-NODO_SIZE EQU 24
-PACKED_NODO_OFFSET_NEXT EQU 8
-PACKED_NODO_OFFSET_CATEGORIA EQU 8
-PACKED_NODO_OFFSET_ARREGLO EQU 8
-PACKED_NODO_OFFSET_LONGITUD EQU 8
-PACKED_NODO_SIZE EQU 8
-LISTA_OFFSET_HEAD EQU 8
+NODO_OFFSET_CATEGORIA EQU 8;uint8_t o sea 1 byte (padding: 7 bytes ya en structs la alineacion se hace en base al tamano del elemento mas grande)
+NODO_OFFSET_ARREGLO EQU 16;puntero, o sea 8 bytes (no hace falta padding)
+NODO_OFFSET_LONGITUD EQU 24;uint32_t o sea 4 bytes (padding: 4 bytes)
+NODO_SIZE EQU 32
+PACKED_NODO_OFFSET_NEXT EQU 0;8 bytes
+PACKED_NODO_OFFSET_CATEGORIA EQU 8;1 byte
+PACKED_NODO_OFFSET_ARREGLO EQU 9;8 bytes
+PACKED_NODO_OFFSET_LONGITUD EQU 17;4 bytes
+PACKED_NODO_SIZE EQU 21
+LISTA_OFFSET_HEAD EQU 0
 LISTA_SIZE EQU 8
-PACKED_LISTA_OFFSET_HEAD EQU 8
+PACKED_LISTA_OFFSET_HEAD EQU 0
 PACKED_LISTA_SIZE EQU 8
 
 ;########### SECCION DE DATOS
